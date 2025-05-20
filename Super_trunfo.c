cf32a0ba2812b1a0381aca020de32b6b68386eb4
2Samy6 <samaravieira57@gmail.com>
2025-05-20 20:22:19 -0300

    Implementa calculos de densidade populacional e PIB per Capita

--- Super_trunfo.c
+++ Super_trunfo.c
@@ -5,11 +5,13 @@ int main() {
 	int populacao1, pontos_turisticos1;
 	char estado1, codigo1[4], cidade1[50];
 	float pib1, area1;
+	float densidade1, pib_per_capita1;
 	
 	//CARTA 2
 	int populacao2, pontos_turisticos2;
 	char estado2, codigo2[4], cidade2[50];
 	float pib2, area2;
+	float densidade2, pib_per_capita2;
 	
 	//Entrada de dados da CARTA 1
 	printf("Carta 1:\n");
@@ -34,6 +36,14 @@ int main() {
 	printf("Quantidade de pontos turisticos: ");
 	scanf("%d", &pontos_turisticos1);
 	
+	// ---Cálculos para a Carta 1 ---
+	//Calcula a densidade populacional: População dividida pela Área
+	densidade1 = (float)populacao1 / area1;
+
+	//Calcula o PIB per Capita: (PIB em bilhões * 1 bilhão) dividido pela População
+	// Multiplicamos por 1,000,000,000.0 para converter o PIB de bilhões para reais
+	pib_per_capita1 = (pib1 * 1000000000.0) / populacao1;
+	
 	//Entrada de dados da CARTA 2
 	printf("\nCarta 2:\n");
 	
@@ -57,6 +67,14 @@ int main() {
 	
 	printf("Quantidade de pontos turisticos: ");
 	scanf("%d", &pontos_turisticos2);
+	
+	// --- Cálculos para a Carta 2 --- 
+	//Calcula a densidade populacional: População dividida pela Área
+	densidade2 = (float)populacao2 / area2;
+	
+	//Calcula o PIB per Capita: (PIB em bilhões * 1 bilhão) dividido pela População
+	// Multiplicamos por 1,000,000,000.0 para converter o PIB de bilhões para reais
+	pib_per_capita2 = (pib2 * 1000000000.0) / populacao2;
 		
 	//EXIBIÇÃO DOS DADOS DA CARTA 1
 	printf("\nCARTA 1:");
@@ -68,6 +86,8 @@ int main() {
 	printf("Area: %.2f km²\n", area1);
 	printf("PIB: %.2f bilhoes de reais\n", pib1);
 	printf("Numero de pontos turisticos: %d\n", pontos_turisticos1);
+	printf("Densidade Populacional: %.2f hab/km²\n", densidade1);
+	printf("PIB per Capita: %.2f reais\n", pib_per_capita1);
 	
 	//ExIBIÇÃO DOS DADOS DA CARTA 2
 	printf("\nCARTA 2:");
@@ -79,6 +99,9 @@ int main() {
 	printf("Area: %.2f km²\n", area2);
 	printf("PIB: %.2f bilhoes de reais\n", pib2);
 	printf("Numero de pontos turisticos: %d\n", pontos_turisticos2);
+	printf("Densidade Populacional: %.2f hab/km²\n", densidade2);
+	printf("PIB per Capita: %.2f reais\n", pib_per_capita2);
+
 	
 	
 	return 0;
